4056072a9571dbb51053a9b5ced5ac226e3bac6c
unknown <riasat01@outlook.com>
2023-09-23 22:57:08 +0600

    filter added

--- src/components/applied-jobs/AppliedJobs.jsx
+++ src/components/applied-jobs/AppliedJobs.jsx
@@ -1,6 +1,7 @@
-import {  useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import { getFromLocalStorage } from "../localstorage/Localstorage";
 import ApppliedJob from "../applied-job/ApppliedJob";
+import { useState } from "react";
 
 const AppliedJobs = () => {
 
@@ -10,18 +11,39 @@ const AppliedJobs = () => {
         // console.log(job.id)
         return appliedJobs.includes(job.id)
     })
-    // console.log(jobsAppllied);
+    const [displayJobs, setDisplayJobs] = useState([]);
+    const handleFilter = filter => {
+        if(filter === `all`){
+            setDisplayJobs(jobsAppllied);
+        }else if(filter === `remote`){
+            const temp = jobsAppllied.filter(job => job.remote_or_onsite === `Remote`);
+            setDisplayJobs(temp);
+        }else{
+            const temp = jobsAppllied.filter(job => job.remote_or_onsite === `Onsite`);
+            setDisplayJobs(temp);
+        }
+    }
 
 
-    
+
 
 
 
     return (
         <div>
-            <h1>Jobs</h1>
+            <section className="flex justify-between">
+                <h1>Jobs</h1>
+                <details className="dropdown mb-32">
+                    <summary className="m-1 btn">Filter</summary>
+                    <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                        <li><Link onClick={() => handleFilter(`all`)}>All</Link></li>
+                        <li><Link onClick={() => handleFilter(`remote`)}>Remote</Link></li>
+                        <li><Link onClick={() => handleFilter(`onsite`)}>On Site</Link></li>
+                    </ul>
+                </details>
+            </section>
             {
-                jobsAppllied.map((job, i) => <ApppliedJob key={i} job={job}></ApppliedJob>)
+                displayJobs.map((job, i) => <ApppliedJob key={i} job={job}></ApppliedJob>)
             }
         </div>
     );
